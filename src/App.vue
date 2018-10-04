b0447e4369aaf3152039d1940e46d6c19a4793b2
Kyle Hatch <ky13h4tch@gmail.com>
2018-10-04 00:48:55 -0600

    Updated UI

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app" class="container">
-    <img src="./assets/dota2.svg" class="logo">
+    <div class="image-container">
+      <!-- <img src="./assets/dota2.svg" class="logo"> -->
+      <img src="./assets/dota2tiny.jpg" class="logo">
+    </div>
     <theme-list />
   </div>
 </template>
@@ -24,10 +27,17 @@ export default {
   box-sizing: border-box;
 }
 
-.container {
-  max-width: 600px;
-  margin: 0 auto;
+body {
+  background-color: #2a3a4a;
+}
 
+.container {
+  max-width: 75%;
+  min-width: 600px;
+  margin: 0 auto;
+  background: #fff;
+  padding: 1em;
+  border-radius: 10px;
 }
 
 #app {
@@ -37,17 +47,39 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
   font-size: 24px;
-}
-
-.logo {
-  display: block;
-  margin: 20px auto;
-  height: 100px;
+  margin-bottom: 2em;
 }
 
 .delete-icon {
   display: block;
   margin: 20px auto;
   height: .8em;
+}
+
+[type="checkbox"]+span:not(.lever) {
+  position: relative;
+  padding-left: 35px;
+  cursor: pointer;
+  display: inline-block;
+  top: 5px;
+  height: 25px;
+  line-height: 25px;
+  font-size: 1rem;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.image-container {
+  background-size: cover;
+  width: 100%;
+  height: 250px;
+  resize: both;
+  overflow: hidden;
+}
+
+img {
+  width: 100%;
 }
 </style>
--- src/components/ThemeCheckAll.vue
+++ src/components/ThemeCheckAll.vue
@@ -24,3 +24,46 @@ export default {
   }
 }
 </script>
+
+<style lang='scss' scoped>
+[type="checkbox"]:checked+span:not(.lever):before {
+    top: -4px;
+    left: -5px;
+    width: 12px;
+    height: 22px;
+    border-top: 2px solid transparent;
+    border-left: 2px solid transparent;
+    border-right: 2px solid #A52A2A !important;
+    border-bottom: 2px solid #A52A2A !important;
+    -webkit-transform: rotate(40deg);
+    transform: rotate(40deg);
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    -webkit-transform-origin: 100% 100%;
+    transform-origin: 100% 100%;
+}
+
+[type="checkbox"].filled-in:checked+span:not(.lever):before {
+    top: 0;
+    left: 1px;
+    width: 8px;
+    height: 13px;
+    border-top: 2px solid transparent;
+    border-left: 2px solid transparent;
+    border-right: 2px solid #fff !important;
+    border-bottom: 2px solid #fff !important;
+    -webkit-transform: rotateZ(37deg);
+    transform: rotateZ(37deg);
+    -webkit-transform-origin: 100% 100%;
+    transform-origin: 100% 100%;
+}
+
+[type="checkbox"].filled-in:checked+span:not(.lever):after {
+    top: 0;
+    width: 20px;
+    height: 20px;
+    border: 2px solid #A52A2A !important;
+    background-color: #A52A2A !important;
+    z-index: 0;
+}
+</style>
